d23669ba961d809bb48ede99aee44b3e603a04bc
poodycliche@hotmail.com <poodycliche@hotmail.com>
2012-04-02 19:52:58 -0400

    Sql: Fix sql for previous commit

--- sql/custom/priest_swd.sql
+++ sql/custom/priest_swd.sql
@@ -1,3 +1,3 @@
--- Glypge de Mot de l'ombre: Mort
-DELETE FROM `spell_proc_event` WHERE `entry`=55682;
-INSERT INTO `spell_proc_event` (`entry`,`SpellFamilyName`,`SpellFamilyMask1`,`procFlags`,`customChance`,`cooldown`) VALUE (55682,6,0x2,0x10000,100,6);
+-- Mot de l'ombre: Mort
+DELETE FROM `spell_script_names` WHERE `spell_id`=32379;
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUE (32379,'spell_pri_shadow_word_death');
