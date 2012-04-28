a29353937f473f085dc117f499234fb91eb2c73b
poodycliche@hotmail.com <poodycliche@hotmail.com>
2012-04-27 20:51:44 -0400

    Sql: add sql for previous commit

---
+++ sql/custom/mage_cauterize.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (86948,86949);
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(86948, 'spell_mage_cauterize'),
+(86949, 'spell_mage_cauterize');
